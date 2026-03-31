722b5c039dab3ec5f80ae718222e1d0524f3e7b4
hatkarabhijeet <hatkarabhijeet@gmail.com>
2026-03-31 17:29:29 +0530

    Create Admin class for admin authentication

---
+++ src/Admin.php
@@ -0,0 +1,15 @@
+<?php
+
+class Admin {
+    public function login($username, $password) {
+        // Logic for admin login
+    }
+
+    public function getById($id) {
+        // Logic to get admin by ID
+    }
+
+    public function changePassword($id, $newPassword) {
+        // Logic to change password
+    }
+}
